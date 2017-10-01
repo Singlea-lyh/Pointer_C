2bc62e0ef5d3c706ae507eb469d5a3e28ae05637
Singlea_lyh <chinese_lyh@163.com>
2017-10-01 14:52:34 +0800

    Add Code
    Array change

--- array.c
+++ array.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	long a[][4] = {1, 2, 3, 4, 5, 6};
+	int c = 1;
+	int **p = (int **)a;
+	int i = 0, j = 0;
+	
+	for(i = 0; i < 3; i++){
+		for(j = 0; j < 3; j++){
+			printf("%-4d", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("%d\n", p[1]);
+	return 0;
+} 
